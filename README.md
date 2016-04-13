2a816e36545349caf69db671b45b2e302ea1f0a6
jiazeng <sophbj93@uw.edu>
2016-04-13 00:28:33 -0700

    added hw1

---
+++ README.md
@@ -0,0 +1,4 @@
+INFO 474 ASSIGNMENT 1
+    Q1：which bacteria does penicilin work the best?
+    Q2: does each anibiotic with best with Gram-positive or with Gram-negative bacteria
+    Q3: 
---
+++ js/main.js
@@ -0,0 +1,43 @@
+// specify data
+// specify layout
+// create chart
+var trace1 = [
+{
+    x:['Aerobacter aerogenes', ,  '', 'Escherichia coli', 'Klebsiella pneumoniae', 'Mycobacterium tuberculosis', 'Proteus vulgaris', 'Pseudomonas aeruginosa',  'Salmonella schottmuelleri', , 'Staphylococcus aureus', , '', ]
+    
+    x:['Brucella abortus', 'Streptococcus hemolyticus', 'Diplococcus pneumoniae', 'Streptococcus viridans', 'Staphylococcus albus', 'Brucella anthracis', 'Salmonella (Eberthella) typhosa', 'Streptococcus fecalis', ]
+    y: [0.001, 0.001, 0.005, 0.005, 0.007, 1, 1, 1, ]
+    type: 'bar'
+}
+];
+
+// var trace1 = {
+//     x: [1, 2, 3, 4],
+//     y: [10, 15, 13, 17],
+//     mode: 'markers',
+//     type: 'scatter'
+// };
+
+// var trace2 = {
+//     x: [2, 3, 4, 5],
+//     y: [16, 5, 11, 9],
+//     mode: 'lines',
+//     type: 'scatter'
+// };
+
+// var trace3 = {
+//     x: [1, 2, 3, 4],
+//     y: [12, 9, 15, 12],
+//     mode: 'lines+markers',
+//     type: 'scatter'
+// };
+
+// var data = [trace1, trace2, trace3];
+
+// var layout = {
+//     title: 'Line Plot',
+//     height: window.height,
+//     width: window.width
+// };
+
+// Plotly.newPlot('graph', data, layout);
